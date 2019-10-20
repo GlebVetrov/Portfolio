08b83d2c1d63353e92eec5138713ba8f1ea35304
Gleb <3696732@mail.ru>
2019-10-20 22:47:06 +0300

    feat: add project theyalow and carousel

--- scripts/education.js
+++ scripts/education.js
@@ -1,11 +1,38 @@
-const educationList = document.querySelector('.education-items');
-const educationButton = document.querySelector('.education-title');
-educationButton.addEventListener('click', () => {
-    if (educationList.classList.contains('education-close')) {
+const education = () => {
+    const educationList = document.querySelector('.education-items');
+    const educationButton = document.querySelector('.education-title');
+
+    const removeClass = () => {
+        educationList.classList.remove('education-open');
+    };
+
+    const showItem = () => {
+        educationList.removeEventListener('animationend', addClass);
         educationButton.innerHTML = `Education <span>&#708;</span>`;
+        educationList.classList.remove('education-none');
+        educationList.classList.add('education-open');
+        educationList.addEventListener('animationend', removeClass);
+    };
+
+    const addClass = () => {
+        educationList.classList.add('education-none');
         educationList.classList.remove('education-close');
-        return;
-    }
-    educationButton.innerHTML = `Education <span>&#709;</span>`;
-    educationList.classList.add('education-close');
-});
+    };
+
+    const hideItem = () => {
+        educationList.removeEventListener('animationend', removeClass);
+        educationButton.innerHTML = `Education <span>&#709;</span>`;
+        educationList.classList.remove('education-open');
+        educationList.classList.add('education-close');
+        educationList.addEventListener('animationend', addClass);
+    };
+
+    educationButton.addEventListener('click', () => {
+        if (educationList.classList.contains('education-none')) {
+            showItem();
+            return;
+        }
+        hideItem();
+    });
+};
+education();
